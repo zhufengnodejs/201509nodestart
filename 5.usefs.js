1677cbcf12d19e2855e468ab79117a203c6a461b
zfpx <zfpx@126.com>
2016-03-01 13:01:03 +0800

    使用模块

--- 5.usefs.js
+++ 5.usefs.js
@@ -4,7 +4,9 @@ var fs = require('fs');
 //同步读取文件的内容，通过返回值接收
 //读了动作会阻塞后续代码的执行
 //1.参数是要读取的文件的文件路径  第二个参数是此文件编码类型
+//Sync就是同步的意思
 var content = fs.readFileSync('./readme.txt','utf8');
+//在上面同步方法没有执行完毕之前不能执行后续的任何代码
 console.log(content);
 console.log('b');
 
--- readme.txt
+++ readme.txt
@@ -1,1 +1,1 @@
-readme
+helloworld
