09611692558d5d35f4db31dcfd4a29c9c409061e
zfpx <zfpx@126.com>
2016-03-01 16:58:11 +0800

    请求响应

--- 8.server.js
+++ 8.server.js
@@ -6,12 +6,15 @@ var http = require('http');
  * response 话筒  代表客户端的响应，可以向响应里写入内容
  */
 var server = http.createServer();
-
+var fs = require('fs');
 //每当客户端请求到来的时候，都会执行回调函数
-server.on('request',function(requerwst,response){
-    response.write('hello');//写入响应
-    response.write('world');
-    response.end();//挂掉电话
+server.on('request',function(request,response){
+    console.log(request.url);
+    //使用node的内置fs模块读取文件的内容
+    var content = fs.readFileSync('./index.html','utf8');
+    //把文件的内容写入响应里面
+    response.write(content);//写入响应
+    response.end();//挂掉电话 结束响应
 });
 //在本地监听8080端口
 //国风美唐4号楼416等着大家过来
